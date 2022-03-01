75c4cfee0589d8e2f47bea8154ccd1a420f1a8ef
vidhishah <vidhishah4756@gmail.com>
2022-03-01 22:40:43 +0530

    detailing random

--- flask_blog/flaskblog/main/routes.py
+++ flask_blog/flaskblog/main/routes.py
@@ -1,5 +1,8 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint,url_for
 from flaskblog.models import Post
+# from flaskblog.main.random_img import ri
+import os
+import random
 
 main = Blueprint('main', __name__)
 @main.route("/blog")
@@ -11,6 +14,23 @@ def blog():
 @main.route("/home")
 def home():
     return render_template('home.html',title='home')
+
+
+# imgs = os.listdir('static/img')
+# imgs = ['img/' + file for file in imgs]
+# @main.route('/index')
+# def index(imgrand = random.sample(imgs,k=5)):
+   
+    
+#     return render_template('home.html', imgrand=imgrand)
+
+
+
+
+
+
+
+
 # @main.route("/contact us")
 # def contactus():
 #     return render_template('contactus.html',title='contactus')
@@ -22,3 +42,8 @@ def home():
 # @main.route("/interpreter")
 # def interpreter():
 #     return render_template('interpreter.html',title='interpreter')
+# @main.route("/")
+# def index():
+#  random_bird = BirdDetails.query.order_by(func.random()).first()
+#  return render_template('main/index.html', bird = random_bird)
+# from birdnet.models import User, Thread, Reply, BirdDetails, db
